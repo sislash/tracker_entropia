8765b13923df359120523b06fb01ce06ddcca07a
sislash <sckavens@gmail.com>
2026-01-25 22:34:25 +0100

    Add files via upload

--- include/globals_stats.h
+++ include/globals_stats.h
@@ -1,7 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   globals_stats.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: entropia-tracker                              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25                                #+#    #+#             */
+/*   Updated: 2026/01/25                                #+#    #+#             */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GLOBALS_STATS_H
 # define GLOBALS_STATS_H
 
 # include <stddef.h>
+
+# define GLOBALS_TOP_MAX 20
 
 typedef struct s_globals_top
 {
@@ -12,28 +26,29 @@ typedef struct s_globals_top
 
 typedef struct s_globals_stats
 {
-    int				csv_has_header;
-    long			data_lines_read;
+    int		csv_has_header;
+    long	data_lines_read;
     
-    long			mob_events;
-    long			craft_events;
-    long			rare_events;
+    long	mob_events;
+    double	mob_sum_ped;
     
-    double			mob_sum_ped;
-    double			craft_sum_ped;
-    double			rare_sum_ped;
+    long	craft_events;
+    double	craft_sum_ped;
     
-    t_globals_top	top_mobs[10];
+    long	rare_events;
+    double	rare_sum_ped;
+    
+    t_globals_top	top_mobs[GLOBALS_TOP_MAX];
     size_t			top_mobs_count;
     
-    t_globals_top	top_crafts[10];
+    t_globals_top	top_crafts[GLOBALS_TOP_MAX];
     size_t			top_crafts_count;
     
-    t_globals_top	top_rares[10];
+    t_globals_top	top_rares[GLOBALS_TOP_MAX];
     size_t			top_rares_count;
 }	t_globals_stats;
 
-/* start_line: comme chasse, si tu veux ignorer N lignes de data (header non compté) */
-int	globals_stats_compute(const char *csv_path, long start_line, t_globals_stats *out);
+int	globals_stats_compute(const char *csv_path, long start_line,
+                          t_globals_stats *out);
 
 #endif
