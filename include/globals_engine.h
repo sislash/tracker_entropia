6366a0f0179cface389013586f043450b3ab5970
sislash <sckavens@gmail.com>
2026-01-27 13:23:12 +0100

    Delete include directory

--- include/globals_engine.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBALS_ENGINE_H
-# define GLOBALS_ENGINE_H
-
-int	globals_run_live(const char *chatlog_path, const char *csv_path,
-                     volatile int *stop_flag);
-int	globals_run_replay(const char *chatlog_path, const char *csv_path,
-                       volatile int *stop_flag);
-
-#endif
--- include/tracker_view.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRACKER_VIEW_H
-# define TRACKER_VIEW_H
-
-# include "tracker_stats.h"
-
-void	tracker_view_print(const t_hunt_stats *s);
-
-#endif
