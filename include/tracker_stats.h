2c31084bbb775814043927015b956e13c357eeff
sislash <sckavens@gmail.com>
2026-02-06 19:52:35 +0100

    Add files via upload

---
+++ include/tracker_stats.h
@@ -0,0 +1,81 @@
+#ifndef TRACKER_STATS_H
+# define TRACKER_STATS_H
+
+# include <stddef.h>
+
+# define TM_TOP_MOBS 10
+# define TM_TOP_LOOT 10
+# define TM_STATS_HAS_MARKUP 1
+
+typedef struct s_top_mob
+{
+	char	name[128];
+	long	kills;
+} 			t_top_mob;
+
+typedef struct s_top_loot
+{
+	char	name[128];
+	double	tt_ped;
+	double	mu_ped;
+	double	total_mu_ped;
+	long	events;
+} 			t_top_loot;
+
+typedef struct s_hunt_stats
+{
+	int			csv_has_header;
+	long		data_lines_read;
+
+	long		kills;
+	long		shots;
+	
+	long		sweat_total;
+	long		sweat_events;
+
+	double		loot_ped;
+	long		loot_events;
+
+	double		expense_ped_logged;
+	long		expense_events;
+
+	double		expense_ped_calc;
+	double		expense_used;
+	int		expense_used_is_logged;
+
+	double		net_ped;
+
+	int		has_weapon;
+	char		weapon_name[128];
+	char		player_name[128];
+	double		cost_shot;
+	double		ammo_shot;
+	double		decay_shot;
+	double		amp_decay_shot;
+	double		markup;
+
+	size_t		mobs_unique;
+	size_t		top_mobs_count;
+	t_top_mob	top_mobs[TM_TOP_MOBS];
+
+	/* ====================================================== */
+	/* TOP LOOT (per item)                                    */
+	/* ====================================================== */
+	size_t		top_loot_count;
+	t_top_loot	top_loot[TM_TOP_LOOT];
+	
+	/* ====================================================== */
+	/* MARKUP (TT / MU / TT+MU)                               */
+	/* Actif uniquement si TM_STATS_HAS_MARKUP est defini     */
+	/* ====================================================== */
+#ifdef TM_STATS_HAS_MARKUP
+	double		loot_tt_ped;
+	double		loot_mu_ped;
+	double		loot_total_mu_ped;
+#endif
+	
+} 			t_hunt_stats;
+
+int	tracker_stats_compute(const char *csv_path, long start_line, t_hunt_stats *out);
+
+#endif
---
+++ include/tracker_view.h
@@ -0,0 +1,9 @@
+#ifndef TRACKER_VIEW_H
+# define TRACKER_VIEW_H
+
+# include "tracker_stats.h"
+
+void	tracker_view_print(const t_hunt_stats *s);
+void	tracker_view_handle_key(int key);
+
+#endif
